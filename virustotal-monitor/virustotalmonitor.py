7b1d11a82a88c018a37ea92830a17a89e6336335
Mike Sconzo <mike.sconzo@airtable.com>
2020-11-13 12:27:13 -0600

    fixed indenting

--- virustotal-monitor/virustotalmonitor.py
+++ virustotal-monitor/virustotalmonitor.py
@@ -26,68 +26,68 @@ class Virustotalmonitor(BotPlugin):
 
 
     def check_lists(self):
-            IPS = os.getenv("VT_IPS")
-            DOMAINS = os.getenv("VT_DOMAINS")
-            APIKEY = os.getenv("VT_APIKEY")
-            headers = {'User-Agent': 'Security Monitor Bot'}
-            ips = list()
-            domains = list()
+        IPS = os.getenv("VT_IPS")
+        DOMAINS = os.getenv("VT_DOMAINS")
+        APIKEY = os.getenv("VT_APIKEY")
+        headers = {'User-Agent': 'Security Monitor Bot'}
+        ips = list()
+        domains = list()
 
-            self.log.info('Waking up to monitor for IPs and Domains')
-            if IPS is not None and IPS != "":
-                ips = [ip.strip() for ip in IPS.split(",")]
-            if DOMAINS is not None and DOMAINS != "":
-                domains = [domain.strip() for domain in DOMAINS.split(",")]
+        self.log.info('Waking up to monitor for IPs and Domains')
+        if IPS is not None and IPS != "":
+            ips = [ip.strip() for ip in IPS.split(",")]
+        if DOMAINS is not None and DOMAINS != "":
+            domains = [domain.strip() for domain in DOMAINS.split(",")]
 
-            indicators = ips + domains
+        indicators = ips + domains
 
-            self.log.info("Monitoring IPs: {}".format(", ".join(ips)))
-            self.log.info("Monitoring Domains: {}".format(", ".join(domains)))
-            for i in indicators:
-                try:
-                    r = requests.get(
-                        "https://www.virustotal.com/vtapi/v2/url/report?apikey={}&resource={}".format(
-                            APIKEY, i
-                        ),
-                        headers=headers
-                    )
-                    if r.status_code == 200:
-                        self.send_to_slack(json.loads(r.text))
-                except Exception as e:
-                    self.log.info("Error requesting ip ({}) check: {}".format(i, e))
+        self.log.info("Monitoring IPs: {}".format(", ".join(ips)))
+        self.log.info("Monitoring Domains: {}".format(", ".join(domains)))
+        for i in indicators:
+            try:
+                r = requests.get(
+                    "https://www.virustotal.com/vtapi/v2/url/report?apikey={}&resource={}".format(
+                        APIKEY, i
+                    ),
+                    headers=headers
+                )
+                if r.status_code == 200:
+                    self.send_to_slack(json.loads(r.text))
+            except Exception as e:
+                self.log.info("Error requesting ip ({}) check: {}".format(i, e))
 
 
     def scan_indicators(self):
-            IPS = os.getenv("VT_IPS")
-            DOMAINS = os.getenv("VT_DOMAINS")
-            APIKEY = os.getenv("VT_APIKEY")
-            headers = {'User-Agent': 'Security Monitor Bot'}
-            data = {'apikey': APIKEY}
-            ips = list()
-            domains = list()
+        IPS = os.getenv("VT_IPS")
+        DOMAINS = os.getenv("VT_DOMAINS")
+        APIKEY = os.getenv("VT_APIKEY")
+        headers = {'User-Agent': 'Security Monitor Bot'}
+        data = {'apikey': APIKEY}
+        ips = list()
+        domains = list()
 
-            self.log.info('Waking up to monitor for IPs and Domains')
-            if IPS is not None and IPS != "":
-                ips = [ip.strip() for ip in IPS.split(",")]
-            if DOMAINS is not None and DOMAINS != "":
-                domains = [domain.strip() for domain in DOMAINS.split(",")]
+        self.log.info('Waking up to monitor for IPs and Domains')
+        if IPS is not None and IPS != "":
+            ips = [ip.strip() for ip in IPS.split(",")]
+        if DOMAINS is not None and DOMAINS != "":
+            domains = [domain.strip() for domain in DOMAINS.split(",")]
 
-            indicators = ips + domains
+        indicators = ips + domains
 
-            self.log.info("Scanning IPs: {}".format(", ".join(ips)))
-            self.log.info("Scanning Domains: {}".format(", ".join(domains)))
-            for i in indicators:
-                try:
-                    data['url'] = i
-                    r = requests.post(
-                        "https://www.virustotal.com/vtapi/v2/url/scan",
-                        headers=headers,
-                        data=data
-                    )
-                    if r.status_code == 200:
-                        self.send_to_slack(json.loads(r.text))
-                except Exception as e:
-                    self.log.info("Error requesting ip ({}) check: {}".format(i, e))
+        self.log.info("Scanning IPs: {}".format(", ".join(ips)))
+        self.log.info("Scanning Domains: {}".format(", ".join(domains)))
+        for i in indicators:
+            try:
+                data['url'] = i
+                r = requests.post(
+                    "https://www.virustotal.com/vtapi/v2/url/scan",
+                    headers=headers,
+                    data=data
+                )
+                if r.status_code == 200:
+                    self.send_to_slack(json.loads(r.text))
+            except Exception as e:
+                self.log.info("Error requesting ip ({}) check: {}".format(i, e))
 
 
     def activate(self):
